444e1d6c72dbed02c32cd49ce9f5e7a4dd94573a
GLeTan <146773953+GLeTan@users.noreply.github.com>
2024-09-12 10:47:35 -0700

    installed jest

--- components/__tests__/wikipediaApiSearch.test.js
+++ components/__tests__/wikipediaApiSearch.test.js
@@ -1,9 +1,6 @@
 //import {searchSite} from '../../app/src/api/inaturalistApi';
 import {fetchWikipediaInfo} from '../../app/src/api/wikipediaApi';
 
-jest.mock('node-fetch', () => require('jest-fetch-mock'));
-import fetch from 'jest-fetch-mock';
-
 describe('fetchSpecies', () => {
     beforeEach(() => {
         fetch.resetMocks();
